878bae0c2b4555eee77fa8c49f4ade9dfd2a621d
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-03-07 14:06:54 -0800

    lesson 6
    made ball move in the direction of the focal point

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     public float rotationSpeed;
-
+    
     // Start is called before the first frame update
     void Start()
     {
